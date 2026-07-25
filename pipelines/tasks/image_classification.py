8c0389ef07fbd4330f1dc1f594e3f64207a411d1
310hz <90894519+310hz@users.noreply.github.com>
2026-07-25 13:51:17 +0900

    image classification update

--- pipelines/tasks/image_classification.py
+++ pipelines/tasks/image_classification.py
@@ -1,12 +1,71 @@
+import torch.nn.functional as F
+from torch.utils.data import IterableDataset
+import torchvision.transforms as transforms
+from datasets import load_dataset
+
 from ..base import Pipeline
+from ..metrics import NamedMetric, CrossEntropy, Accuracy
+
+
+class ImageDataset(IterableDataset):
+    def __init__(self, ds):
+        self.ds = ds
+        self.transform = transforms.Compose([
+            transforms.Resize(224),
+            transforms.RandomCrop(224),
+            transforms.ToTensor(),
+        ])
+
+    def __iter__(self):
+        for sample in self.ds:
+            img = sample["image"]
+            label = sample["label"]
+            img = self.transform(img)
+            yield img, label
 
 
 class ImageClassificationPipeline(Pipeline):
     def get_dataset(self):
-        pass
+        ds_train = load_dataset(
+            "ILSVRC/imagenet-1k",
+            split="train",
+            streaming=True,
+        )
+        ds_test = load_dataset(
+            "ILSVRC/imagenet-1k",
+            split="validation[:10000]",
+        )
+        return ds_train, ds_test, ImageDataset
 
-    def calc_loss(self, batch):
-        pass
+    def get_metrics(self):
+        return NamedMetric({
+            "cross_entropy": CrossEntropy(),
+            "accuracy": Accuracy(),
+        })
 
-    def evaluate(self):
-        pass
+    def forward(self, batch):
+        imgs, targets = batch
+        imgs = imgs.to(self.device)
+        targets = targets.to(self.device)
+        predicts = self.model(imgs)
+        predicts = predicts.view(-1, predicts.size(-1))
+        targets = targets.view(-1)
+        return predicts, targets
+
+    def loss_fn(self, predicts, targets):
+        loss = F.cross_entropy(predicts, targets)
+        return loss
+
+
+class ImageClassificationNanoPipeline(ImageClassificationPipeline):
+    def get_dataset(self):
+        ds_train = load_dataset(
+            "uoft-cs/cifar10",
+            split="train",
+            streaming=True,
+        )
+        ds_test = load_dataset(
+            "uoft-cs/cifar10",
+            split="test[:100]",
+        )
+        return ds_train, ds_test, ImageDataset
