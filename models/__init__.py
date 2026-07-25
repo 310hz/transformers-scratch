90bc6c5c0f76ae8d37ea05eb19a1d5bead7ab3f2
310hz <90894519+310hz@users.noreply.github.com>
2026-07-26 00:54:54 +0900

    add resnet

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,5 @@ from .vanilla_transformer import VanillaTransformer
 from .gpt2 import GPT2Encoder, GPT2
 
 from .vgg import VGG16
+from .resnet import ResNet34, ResNet50
 from .vision_transformer import VisionTransformerEncoder, VisionTransformer
---
+++ models/resnet.py
@@ -0,0 +1,114 @@
+import torch.nn as nn
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, c_in, c_out, downsample=True):
+        super().__init__()
+        stride = 2 if downsample else 1
+        self.conv = nn.Sequential(
+            nn.Conv2d(c_in, c_out, kernel_size=3, padding=1, stride=stride),
+            nn.BatchNorm2d(c_out),
+            nn.ReLU(),
+            nn.Conv2d(c_out, c_out, kernel_size=3, padding=1),
+            nn.BatchNorm2d(c_out),
+        )
+        if c_in != c_out or downsample:
+            self.skip = nn.Conv2d(c_in, c_out, kernel_size=1, stride=stride)
+        else:
+            self.skip = nn.Identity()
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.relu(self.conv(x) + self.skip(x))
+
+
+class BasicBlocks(nn.Module):
+    def __init__(self, n_layers, c_in, c_out, downsample=True):
+        super().__init__()
+        self.net = nn.Sequential(
+            BasicBlock(c_in, c_out, downsample=downsample),
+            *[BasicBlock(c_out, c_out, downsample=False) for _ in range(n_layers - 1)],
+        )
+
+    def forward(self, x):
+        print(f"Input shape: {x.shape}")
+        return self.net(x)
+
+
+class BottleneckBlock(nn.Module):
+    def __init__(self, c_in, c_mid, c_out, downsample=True):
+        super().__init__()
+        stride = 2 if downsample else 1
+        self.conv = nn.Sequential(
+            nn.Conv2d(c_in, c_mid, kernel_size=1, stride=stride),
+            nn.BatchNorm2d(c_mid),
+            nn.ReLU(),
+            nn.Conv2d(c_mid, c_mid, kernel_size=3, padding=1),
+            nn.BatchNorm2d(c_mid),
+            nn.ReLU(),
+            nn.Conv2d(c_mid, c_out, kernel_size=1),
+            nn.BatchNorm2d(c_out),
+        )
+        if c_in != c_out or downsample:
+            self.skip = nn.Conv2d(c_in, c_out, kernel_size=1, stride=stride)
+        else:
+            self.skip = nn.Identity()
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.relu(self.conv(x) + self.skip(x))
+
+
+class BottleneckBlocks(nn.Module):
+    def __init__(self, n_layers, c_in, c_mid, c_out, downsample=True):
+        super().__init__()
+        self.net = nn.Sequential(
+            BottleneckBlock(c_in, c_mid, c_out, downsample=downsample),
+            *[BottleneckBlock(c_out, c_mid, c_out, downsample=False) for _ in range(n_layers - 1)],
+        )
+
+    def forward(self, x):
+        print(f"Input shape: {x.shape}")
+        return self.net(x)
+
+
+class ResNet34(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            BasicBlocks(3, c_in=64, c_out=64, downsample=False),
+            BasicBlocks(4, c_in=64, c_out=128),
+            BasicBlocks(6, c_in=128, c_out=256),
+            BasicBlocks(3, c_in=256, c_out=512),
+            nn.AvgPool2d(kernel_size=7),
+            nn.Flatten(),
+            nn.Linear(512, n_classes),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class ResNet50(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            BottleneckBlocks(3, c_in=64, c_mid=64, c_out=256, downsample=False),
+            BottleneckBlocks(4, c_in=256, c_mid=128, c_out=512),
+            BottleneckBlocks(6, c_in=512, c_mid=256, c_out=1024),
+            BottleneckBlocks(3, c_in=1024, c_mid=512, c_out=2048),
+            nn.AvgPool2d(kernel_size=7),
+            nn.Flatten(),
+            nn.Linear(2048, n_classes),
+        )
+
+    def forward(self, x):
+        return self.net(x)
